50ec125d7a5d3f83060c0ed044ee9885698a4a1c
carlislerainey <carlislerainey@gmail.com>
2014-10-16 07:08:34 -0400

    remove the base if all bases are one

--- R/sci_notation.R
+++ R/sci_notation.R
@@ -5,6 +5,12 @@ sci_notation <- function(x, digits = 1) {
   if (!x) return(0)
   exponent <- floor(log10(x))
   base <- round(x / 10^exponent, digits)
-  as.expression(substitute(base %*% 10^exponent, 
-                           list(base = base, exponent = exponent)))
+  if (sum(base == 1) == length(base)) {
+    as.expression(substitute(10^exponent, 
+                             list(exponent = exponent)))
+  } else {
+    as.expression(substitute(base %*% 10^exponent, 
+                             list(base = base, exponent = exponent)))
+  }
+
 }
